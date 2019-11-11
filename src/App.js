ca3a90346a8fb79e92d03787ff466ffd4331545a
Cavermore <44120986+HushterSec-M@users.noreply.github.com>
2019-11-12 00:12:01 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React from "react";
+import Info from "./components/info";
+import Form from "./components/form";
+import Weather from "./components/weather";
+
+const API_KEY = "0c6ee5ca545af6e543bc9ef7473e63f2";
+
+class App extends React.Component {
+
+    state = {
+        temp: undefined,
+        city: undefined,
+        country: undefined,
+        pressure: undefined,
+        sunset: undefined,
+        error: undefined
+    }
+
+    gettingweather = async (e) => {
+        e.preventDefault();
+        const city = e.target.elements.city.value
+
+        if (city){
+            const api_url = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`);
+            const data = await api_url.json();
+
+            var sunset = data.sys.sunset;
+            var date = new Date();
+            date.setTime(sunset);
+            var sunset_date = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+
+            this.setState({
+                temp: data.main.temp,
+                city: data.name,
+                country: data.sys.country,
+                pressure: data.main.pressure,
+                sunset: sunset_date,
+                error: undefined
+            });
+        } else {
+            this.setState({
+                temp: undefined,
+                city: undefined,
+                country: undefined,
+                pressure: undefined,
+                sunset: undefined,
+                error: "Введите название города на английском"
+            });
+        }
+    }
+
+    render() {
+        return (
+            <div className="wrapper">
+                <div className="main">
+                    <div className="row">
+                        <div className="col-sm-5 info">
+                            <Info />
+                        </div>
+                        <div className="col-sm-7 form">
+                            <Form weatherMethod={this.gettingweather} />
+                            <Weather
+                                temp={this.state.temp}
+                                city={this.state.city}
+                                country={this.state.country}
+                                pressure={this.state.pressure}
+                                sunset={this.state.sunset}
+                                error={this.state.error}
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/info.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+//Упрощается как weather
+class Info extends React.Component {
+    render(){
+        return (
+            <div>
+                <h2>Погодное приложение</h2>
+                <p>Нужная информация о сегодняшнем дне</p>
+            </div>
+        );
+    }
+}
+
+export default Info;
